b0c656f105891b619fb723f60e655a358eda410f
rahmanabdulquadir <rahmanaq777@gmail.com>
2025-03-19 14:41:40 +0600

    dynamic and reusable sidebar created and layout fixing done

--- src/pages/consumer/RootConsumer.tsx
+++ src/pages/consumer/RootConsumer.tsx
@@ -1,10 +1,23 @@
 import Sidebar from "@/components/consumer/standard/Sidebar";
 import { Outlet } from "react-router-dom";
+import { FaHome } from "react-icons/fa";
+import { MdHistory, MdSettings, MdLogout } from "react-icons/md";
+import { BsBuilding } from "react-icons/bs";
+import { IoPeople } from "react-icons/io5";
+
+const consumerMenu = [
+  { path: "/consumer/dashboard", label: "Dashboard", icon: FaHome },
+  { path: "/consumer/buildingInformation", label: "Building Information", icon: BsBuilding },
+  { path: "/consumer/certifiedAssociates", label: "Certified Associates", icon: IoPeople },
+  { path: "/consumer/history", label: "History", icon: MdHistory },
+  { path: "/consumer/settings", label: "Settings", icon: MdSettings },
+  { path: "/logout", label: "Logout", icon: MdLogout },
+];
 
 const RootConsumer = () => {
   return (
-    <div className="flex">
-      <Sidebar />
+    <div className="flex px-20">
+      <Sidebar menuItems={consumerMenu} />
       <div className="flex-1 p-4">
         <Outlet />
       </div>
